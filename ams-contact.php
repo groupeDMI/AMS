8103cb2310495571b35a3e0cec2c2493a24c286f
whispyy <jfdurk@gmx.fr>
2019-09-25 18:09:23 -0400

    core: last snapshot of ams-ressorts.com

---
+++ ams-contact.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//FR" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>AMS ressorts | contact</title>
+<meta name="Description" content="Formulaire de contact d'AMS fabricant de ressorts" />
+<meta name="http-language" content="fr" />
+<meta http-equiv="Reply-to" content="contact@solu-site-creation.com" />
+<meta name="author" content="Didier Piochel http://www.solu-site-creation.com" />
+<meta name="robots" content="all" />
+<meta name="robots" content="index follow" />
+<meta name="revisit-after" content="7 days" />
+<meta name="distribution" content="Global" />
+<meta http-equiv="Content-Language" content="fr" />
+<meta http-equiv="Window-target" content="_top" />
+<meta name="MSSmartTagsPreventParsing" content="TRUE" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<style type="text/css" media="screen">
+<!--
+INPUT { color: #000; font-size: 11px; font-family: verdana, Arial, Helvetica, Geneva, Swiss, SunSans-Regular; background-color: #FFFFFF }
+SELECT { color: #000; font-size: 11px; font-family: verdana, Arial, Helvetica, Geneva, Swiss, SunSans-Regular; background-color: #FFFFFF }
+TEXTAREA { color: #000; font-size: 11px; font-family: verdana, Arial, Helvetica, Geneva, Swiss, SunSans-Regular; background-color: #FFFFFF }
+.txterror { color: red; font-size: 12px; font-family: Verdana, Arial, Helvetica, Geneva, Swiss, SunSans-Regular }
+.txtform { color: black; font-size: 12px; font-family: Verdana, Arial, Helvetica, Geneva, Swiss, SunSans-Regular }
+-->
+</style>
+
+</head>
+
+<body>
+<table width="800" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#575757">
+  <tr>
+    <td height="20" colspan="3" bgcolor="#575757"><h1>Comment trouver AMS ?</h1></td>
+  </tr>
+  <tr>
+    <td height="196" colspan="3">
+    
+<div align="center">
+  <OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+          codebase="http://active.macromedia.com/flash2/cabs/swflash.cab#version=4,0,0,0"
+          ID=ams WIDTH=798 HEIGHT=196>
+          <PARAM NAME=movie VALUE="ams.swf">
+          <PARAM NAME=quality VALUE=high>
+        <PARAM NAME=wmode VALUE=transparent>
+          <PARAM NAME=loop VALUE=true>
+          <EMBED src="ams.swf" loop=true quality=high  wmode=transparent 
+           WIDTH=798 HEIGHT=196 TYPE="application/x-shockwave-flash"
+           PLUGINSPAGE="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+          </EMBED>
+  </OBJECT>
+  </div>
+      
+    </td>
+  </tr>
+</table>
+  <table width="800" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="22" background="images/left-shadow.jpg">&nbsp;</td>
+    <td width="189" height="25" class="navbar"><a href="http://www.ams-ressorts.com" class="navbar">Accueil</a></td>
+    <td width="189" height="25" class="navbar"><a href="ams-ressorts-historique.html" class="navbar">A propos d'AMS</a></td>
+    <td width="189" height="25" class="navbar"><a href="ams-plan-acces.html" target="_blank" class="navbar">Plan d'acc&egrave;s</a></td>
+    <td width="189" height="25" class="navbar-off">Nous contacter</td>
+    <td width="22" background="images/right-shadow.jpg">&nbsp;</td>
+  </tr>
+  </table>
+  <table width="800" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="22" background="images/left-shadow.jpg">&nbsp;</td>
+    <td width="756" height="200" align="left" valign="middle" background="images/bg-text-logo.jpg"><table width="730" border="0" align="center">
+      <tr>
+        <td width="438">
+		
+<form action='' method='post' name='Form'>
+<table width='100%' border='0' cellspacing='1' cellpadding='1'>
+<tr><td colspan='2'><p class="txterror">&nbsp;</p>
+  <p class="text-bordo">Les champs marqués d'un * sont obligatoires</p>
+  </td>
+</tr>
+<tr><td align='right' width='30%'><span class='txtform'>Nom</span><span class="text-bordo">*</span><span class='txtform'> :</span></td>
+<td><input type='text' style='width:200 ;' name='f_1' value='' size='24' border='0'></td></tr>
+<tr><td align='right' width='30%'><span class='txtform'>Prénom</span><span class="text-bordo">*</span><span class='txtform'> :</span></td>
+<td><input type='text' style='width:200 ;' name='f_2' value='' size='24' border='0'></td></tr>
+<tr><td align='right' width='30%'><span class='txtform'>Société</span><span class="text-bordo">*</span><span class='txtform'> :</span></td>
+<td><input type='text' style='width:200 ;' name='f_3' value='' size='24' border='0'></td></tr>
+<tr><td align='right' width='30%'><span class='txtform'>E-mail</span><span class="text-bordo">*</span><span class='txtform'> :</span></td>
+<td><input type='text' style='width:200 ;' name='f_4' value='' size='24' border='0'></td></tr>
+<tr><td align='right' width='30%'><span class='txtform'>Tél :</span></td><td><input type='text' style='width:200 ;' name='f_5' value='' size='24' border='0'></td></tr>
+<tr><td align='right' width='30%'><span class='txtform'>Message</span><span class="text-bordo">*</span><span class='txtform'> :</span></td>
+<td><textarea style='width:360 ;' name='f_6' rows='6' cols='40'></textarea></td></tr>
+<tr><td align='right' width='30%'></td><td><input type='submit' name='submit' value='Envoyer' border='0'></td></tr>
+</table>
+</form>
+
+	</td>
+        <td width="282"><div align="center" class="text-bordo"><strong>AMS<br />
+  &quot;Ateliers M&eacute;caniques de Saucourt&quot;<br />
+          20 Rue de Feuqui&egrave;res<br />
+          Saucourt &ndash; 80390 NIBAS<br />
+          T&eacute;l&nbsp;: 03 22 30 41 90<br />
+          Fax&nbsp;: 03 22 26 46 01</strong><br />
+        contact.ams[at]ressorts.net</div></td>
+      </tr>
+    </table>
+    <br /></td>
+    <td width="22" background="images/right-shadow.jpg">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="75" colspan="3" valign="top" background="images/footer-shadow-gray.jpg" class="footer"><div align="center"><br />
+  &copy; 2008-2011 AMS (Ateliers M&eacute;caniques de Saucourt) | <a href="ams-mentions-legales.html" class="footer-solu-site">Mentions l&eacute;gales</a> <br />
+    <span class="footer-solu-site"><a class="footer-solu-site" href="http://www.solu-site-creation.com" title="creation site">creation site internet</a><a class="footer-solu-site" href="http://www.solu-site-creation.com" title="creation site"><img src="images/copyright-solu-site-creation.jpg" alt="creation site picardie" name="SoluSite" width="67" height="12" border="0" align="absmiddle" /></a></span></div></td>
+  </tr>
+</table>
+
+<!-- phpmyvisites -->
+<a href="http://www.phpmyvisites.us/" title="stats"
+onclick="window.open(this.href);return(false);"><script type="text/javascript">
+<!--
+var a_vars = Array();
+var pagename='';
+
+var phpmyvisitesSite = 10;
+var phpmyvisitesURL = "http://www.solu-site-creation.com/phpmv2/phpmyvisites.php";
+//-->
+</script>
+<script language="javascript" src="http://www.solu-site-creation.com/phpmv2/phpmyvisites.js" type="text/javascript"></script>
+<object><noscript><p>stats
+<img src="http://www.solu-site-creation.com/phpmv2/phpmyvisites.php" alt="Statistics" style="border:0" />
+</p></noscript></object></a>
+<!-- /phpmyvisites --> 
+
+</body>
+</html>
